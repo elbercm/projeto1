adf9cceebcc8b604e12f1afd1d7a9ad64f9ebc6b
Elber <elber.cm@hotmail.com>
2022-10-14 21:36:40 -0300

    Renomeado o Projeto e feito o ORM

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |',
+    })
+
+
+def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
