0fec491cebe2c8e9fa6e09d31279192b73bb2097
Elber <elber.cm@hotmail.com>
2022-11-03 17:19:46 -0300

    Testes de category, recipes e author

--- recipes/views.py
+++ recipes/views.py
@@ -29,10 +29,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ).order_by('-id').first()
+    recipe = get_list_or_404(Recipe, pk=id, is_published=True)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
